8742f1d731d17db04950352a143d43560501859a
Lina María Uribe Montoya - Ceiba Software <lina.uribe@ceiba.com.co>
2021-01-25 15:57:12 -0500

    Init

---
+++ src/main/java/com/ceiba/concessionnaire/ConcessionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.concessionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionnaireApplication.class, args);
+	}
+
+}
